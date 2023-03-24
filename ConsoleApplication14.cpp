0928f7095cc2941d33fe59226f8ca4e7e8b5bfce
Mikota00 <126934810+Mikota00@users.noreply.github.com>
2023-03-24 23:52:19 +1000

    Массивы данных
    Массивы данных позволяют хранить большой объем информации в одном месте
    пример char""

---
+++ ConsoleApplication14.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+
+int main(){
+    char anime[] = "I watch anime  and I love it very much";
+
+     anime[8] = 'm';
+     anime[9] = 'o';
+     anime[10] = 'v';
+     anime[11] = 'i';
+     anime[13] = 's';
+
+    printf("%s\n", anime);    
+
+}
