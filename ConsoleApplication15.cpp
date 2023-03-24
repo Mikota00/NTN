80f168851525f0385d6b75c95428a1589e7cb670
Mikota00 <126934810+Mikota00@users.noreply.github.com>
2023-03-25 00:10:39 +1000

    Массивы данных
    Массивы данных позволяют хранить большой объем информации в одном месте
    пример с числами int, float

---
+++ ConsoleApplication15.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+    int gogo[] = { 235,435,543,431,232 };
+
+    gogo[1] = 10;
+
+    printf("%d\n", gogo[1]);
+
+    float nono[5];
+
+    nono[0] = 2.4;
+    nono[1] = 3,5;
+    nono[2] = 1,5;
+    nono[3] = 7.6;
+    nono[4] = 8,9;
+
+    printf("%.2f", nono[3]);
+
+}
+
